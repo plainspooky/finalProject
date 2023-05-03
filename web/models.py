d5ae0dadef133cc89528a974103df414ecfaf27d
Andrimarciely Souza <andrimarciely@gmail.com>
2023-05-02 21:13:26 -0400

    fix - status of model event

--- web/models.py
+++ web/models.py
@@ -10,9 +10,16 @@ class Event(models.Model):
          )
     inital_date = models.DateField()
     final_date = models.DateField()
+
     responsible_technician = models.CharField(max_length=80)
     description = models.CharField(max_length=100)
-    priority = models.CharField(max_length=1)
+    priority = models.CharField(max_length=1,choices=priorities_list)
 
+    '''status: aberto (true) , fechado (false)'''
+    oppened_status = models.BooleanField(
+        default=True,
+        help_text="aberto ou fechado?",
+        verbose_name="status"
+    )
     def __str__(self):
         return self.description

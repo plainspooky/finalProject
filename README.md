5e474b174265d13bc82986897f8db34b2d386462
Andrimarciely Souza <andrimarciely@gmail.com>
2023-05-02 21:05:06 -0400

    model event

---
+++ README.md
@@ -0,0 +1,9 @@
+# finalProject
+Projeto de Conclusão de Curso - SIMPLIMS - Um sistema web do tipo LIMS voltado para a área ambiental.
+
+Para ele funcionar
+- git clone https://github.com/Andrimarciely/finalProject 
+- python3 -m venv .venv
+- source .venv/bin/activate
+- pip install -r requirements.txt
+- python manage.py migrate
--- web/models.py
+++ web/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Event(models.Model):
+    priorities_list = (
+        ('0', 'Sem prioridade'),
+        ('1', 'Normal'),
+        ('2', 'Urgente'),
+        ('3', "Muito Urgente"),
+         )
+    inital_date = models.DateField()
+    final_date = models.DateField()
+    responsible_technician = models.CharField(max_length=80)
+    description = models.CharField(max_length=100)
+    priority = models.CharField(max_length=1)
+
+    def __str__(self):
+        return self.description

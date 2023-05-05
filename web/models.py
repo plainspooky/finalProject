89ae7e2b4a159d8ec9eacf4eaef1b17a607f3c5b
Andrimarciely Souza <andrimarciely@gmail.com>
2023-05-05 16:26:58 -0400

    fix - fields in form event

--- web/models.py
+++ web/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class Event(models.Model):
     priorities_list = (
@@ -7,7 +8,7 @@ class Event(models.Model):
         ('1', 'Normal'),
         ('2', 'Urgente'),
         ('3', "Muito Urgente"),
-         )
+    )
 
     inital_date = models.DateField(
         help_text="data inicial",
@@ -36,5 +37,6 @@ class Event(models.Model):
         help_text="aberto ou fechado?",
         verbose_name="status",
     )
+
     def __str__(self):
         return self.description

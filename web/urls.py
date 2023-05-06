7bce37ae52af8ebe213d2ba8ebb4c013935231b0
Andrimarciely Souza <andrimarciely@gmail.com>
2023-05-06 10:57:46 -0400

    fix - event delete, update, create

--- web/urls.py
+++ web/urls.py
@@ -5,4 +5,14 @@ from . import views
 urlpatterns = [
     path("", views.EventListView.as_view(), name="event-list"),
     path("event/new", views.EventCreateView.as_view(), name="event-create"),
+    path(
+        "event/<int:pk>/edit",
+        views.EventUpdateView.as_view(),
+        name="event-update",
+    ),
+    path(
+        "event/<int:pk>/delete",
+        views.EventDeleteView.as_view(),
+        name="event-delete",
+    ),
 ]

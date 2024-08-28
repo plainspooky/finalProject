2c414e97cce41a09823f54150f0d2ce80ac50f7e
Andri em Casa <andrimarciely@gmail.com>
2024-08-28 16:10:46 -0400

    new feat - navbar

--- web/urls.py
+++ web/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    #path('', views.home, name="home"),
     path("", views.EventListView.as_view(), name="event-list"),
     path("event/new", views.EventCreateView.as_view(), name="event-create"),
     path(
